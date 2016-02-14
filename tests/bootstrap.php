275fc537f57efe053c1547f75d946e3fb76f2d24
Scott Simpson <sasimpson@gmail.com>
2016-02-14 12:20:47 -0600

    remove line from bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,5 +4,4 @@ $loader = @include __DIR__.'/../vendor/autoload.php';
 if (!$loader) {
     $loader = require __DIR__.'/../vendor/autoload.php';
 }
-// $loader->addPsr4('React\\Promise\\', __DIR__);
 ?>
